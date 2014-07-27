02db74f0c8a276f0f4729a03cb90f3d1183d85ef
Edouard Lopez <dev+git@edouard-lopez.com>
2014-07-28 00:45:05 +0200

    refactor(js): create IIFE and object for the app, improve SoC

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,24 +2,42 @@
  * User: ed8
  * Date: 7/27/14
  */
-(function (window, document) {
-    $('.btn-action').click( function () {
-        var action = this.id;
 
-        console.info(action);
-        var request = $.ajax({
-            url: "/api/"+action,
-            cache: false
-        });
-        request.done(function(_) {
-            console.log(_);
-            $(".stdout").append(_);
-        });
+var instance = {
+	apiCall: function (action) {
+		console.info('x'+action);
+		var request = $.ajax({
+				url: "/api/"+action,
+				cache: false
+		});
+		request.done(function(_) {
+				console.log(_);
+				$(".stdout").append(_);
+		});
 
-        request.fail(function(jqXHR, _) {
-            console.error(jqXHR, _);
-            $(".stdout").append(_);
+		request.fail(function(jqXHR, _) {
+				console.error(jqXHR, _);
+				$(".stdout").append(_);
+		});
+		return false;
+	},
+
+
+	init: function () {
+		var self = this;
+        $('#service .btn-action').on( "click", function() {
+            self.apiCall(this.id);
         });
-        return false;
-    });
-}(window, document));
+        $('#tunnels .btn-action').on( "click", function() {
+            self.apiCall(this.id);
+        });
+        $('.btn-clear').on( "click", function() {
+            $('.stdout').html('');
+        });
+    }
+};
+
+
+(function (window, document, _) {
+	_.init();
+}(window, document, instance));
