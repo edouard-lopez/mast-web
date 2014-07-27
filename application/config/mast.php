6a0f6e2a18bd6a5ce8cc8d56380f766aada28072
Edouard Lopez <dev+git@edouard-lopez.com>
2014-07-27 22:59:05 +0200

    feat(ci,config): add project config file config/mast.php
    * SERVICE_ACTIONS: list of white-listed service's actions
    * SERVICE_HELPERS: list of white-listed makefile's helpers
    * PROJECT: brand

---
+++ application/config/mast.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: ed8
+ * Date: 7/27/14
+ */
+
+$config['SERVICE_ACTIONS'] = array(
+    'start',
+    'stop',
+    'restart',
+    'list',
+    'list-log'
+);
+$config['SERVICE_HELPERS'] = array(
+    'add-host',
+    'remove-host'
+);
+$config['PROJECT'] = 'Mast web';
+$config['PROJECT.html'] = '<abbr title="Multiple Auto-SSH Tunnels web" class="initialism">MAST</abbr>-web';
