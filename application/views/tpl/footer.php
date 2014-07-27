cc6f84c68dce52ffa0e3eeb0e87020a25c3f832b
Edouard Lopez <dev+git@edouard-lopez.com>
2014-07-27 12:21:21 +0200

    feat(tpl): add header/footer template

---
+++ application/views/tpl/footer.php
@@ -0,0 +1,12 @@
+
+      <footer>
+        <p>&copy; Company 2014</p>
+      </footer>
+    </div> <!-- /container -->        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="resources/js/vendor/jquery-1.11.0.min.js"><\/script>')</script>
+
+        <script src="resources/js/vendor/bootstrap.min.js"></script>
+
+        <script src="resources/js/main.js"></script>
+    </body>
+</html>
