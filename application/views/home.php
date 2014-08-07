0397182a88257277451f9b1b756cb6ef92681a42
Sctfic (Alban LOPEZ) <alban.lopez+github@gmail.com>
2014-08-08 00:20:33 +0200

    feat(viewer/home.php):add script code for download

--- application/views/home.php
+++ application/views/home.php
@@ -85,7 +85,7 @@
                                         <?php foreach ($this->config->item('SERVICE_CH_HELPERS') as $action => $props): ?>
                                             <li>
                                                 <button type="button" id="<?= $action ?>" class="btn btn-xs <?= $props['class'] ?> glyphicon <?= $props['icon'] ?>"
-                                                        data-script='<?=json_encode(array('site'=>$site,'vps'=>$_SERVER['HTTP_HOST'],'port'=>$channel['listenPort'], 'channelComment'=>'Commemtaire HP'))?>'>
+                                                        data-script='<?=json_encode(array('site'=>$site,'vps'=>$_SERVER['HTTP_HOST'],'port'=>$channel['listenPort'], 'imp'=>$channel['remoteHost'], 'channelComment'=>'NameInCommentConfig'))?>'>
                                                     <span><?= ucfirst(i18n($this, $action)) ?></span>
                                                 </button>
                                             </li>
@@ -130,5 +130,46 @@
     <pre class="stdout"><?= $this->shell->run("/etc/init.d/mast status"); ?></pre>
 </div>
 </div>
+<script type="text/javascript">
+// afichage en blanc sur fond noir
+var BAT_install_code = "\
+# DOS – Batch XP+\
+# Creaton du port GZ\
+cscript C:\\Windows\\System32\\Printing_Admin_Scripts\\fr-FR\\prnport.vbs -a -o raw -h %vps% -r \"GZ_%vps%_%port%\" -n %port%\
+# Creaton du port de secour en direct\
+cscript C:\\Windows\\System32\\Printing_Admin_Scripts\\fr-FR\\prnport.vbs -a -o raw -h %imp% -r \"DIRECT_%imp%_%port%\"\
+# Install du driver\
+cscript C:\\Windows\\System32\\Printing_Admin_Scripts\\fr-FR\\Prndrvr.vbs -a -m \"MS Publisher Color Printer\"\
+# Creation de l’objet imprimante\
+cscript C:\\Windows\\System32\\Printing_Admin_Scripts\\fr-FR\\Prnmngr.vbs -a -p \"%name%\" -r \"GZ_%vps%_%port%\" -m \"MS Publisher Color Printer\"\
+# ajout des infos : emplacement et commentaire\
+cscript C:\\Windows\\System32\\Printing_Admin_Scripts\\fr-FR\\Prncnfg.vbs -t -p \"%name%\" -l \"%site%\" -m \"GZ par tunnel SSH (%UTC%)`n%imp% par le canal %port%\"\
+";
 
+function BAT(){
+
+}
+
+// affichage en blanc sur fond bleu foncé
+var PS1_install_code = "\
+# Powershell V4+ Windows 2012R2\
+# Creaton du port\
+Add-PrinterPort -Name \"GZ_%vps%_%port%\" -PrinterHostAddress \"%vps%\" -PortNumber %port%\
+# Creaton du port de secour en direct\
+Add-PrinterPort -Name \"DIRECT_%imp%_%port%\" -PrinterHostAddress \"%imp%\"\
+# Install du driver\
+if (Get-PrinterDriver -Name \"MS Publisher Color Printer\") {\
+    Write-Host \"Pilote Generique deja present\"\
+} else {\
+    Write-Host \"Installation du pilote generique : MS Publisher Color Printer\"\
+    Add-PrinterDriver \"MS Publisher Color Printer\"\
+}\
+# Creation de l’objet imprimante\
+Add-Printer -name \"%name%\" -PortName \"GZ_%vps%_%port%\" -Location \"%site%\" -Comment \"GZ par tunnel SSH (%UTC%)\"  -DriverName \"MS Publisher Color Printer\"\
+";
+
+function PS1(){
+
+}
+</script>
 <?php require TPL_PATH . '/footer.php'; ?>
