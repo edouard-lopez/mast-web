88d2dcadd70ab1dfc1a7b1b9a65723902af0cf09
Sctfic (Alban LOPEZ) <alban.lopez+github@gmail.com>
2014-10-01 22:01:46 +0200

    fix(tooltip):issue #30

--- application/views/home.php
+++ application/views/home.php
@@ -33,7 +33,8 @@
                 <div id="tunnel_<?=md5($tunnel)?>" class="panel panel-default tunnel" data-tunnel='<?= json_encode($tunnelConfig,JSON_HEX_APOS | JSON_HEX_QUOT) ?>'>
                     <div class="panel-heading">
                         <h4 class="panel-title">
-                            <i id="x<?=md5($tunnelConfig['remoteHost'].':'.$tunnelConfig['remotePort'])?>" class='hide' data-toggle="tooltip" data-placement="top"> </i>
+                            <i id="x<?=md5($tunnelConfig['remoteHost'].':'.$tunnelConfig['remotePort'])?>" class='hide'
+                            data-html="true" data-toggle="tooltip" data-placement="top" title="Host Unreachable!"> </i>
                             <a data-toggle="collapse" data-parent="#accordion"
                                href="#collapse-<?= $tunnel ?>"> <?= $tunnel . ' - ' . $tunnelConfig['remoteHost'] ?></a>
                             <ul class="nav nav-pills pull-right">
@@ -82,8 +83,8 @@
                                 <?php foreach ($tunnelConfig['channels'] as $channel) { ?>
                                     <li id="channel_<?=md5($channel['remoteHost'])?>" data-channel='<?= json_encode($channel,JSON_HEX_APOS | JSON_HEX_QUOT) ?>' class='channel'>
                                         <span>
-                                            <i id="x<?=md5($channel['remoteHost'].':'.$channel['remotePort'])?>" class='hide'
-                                               data-toggle="tooltip" data-placement="top"></i>
+                                            <i id="x<?=md5($channel['remoteHost'].':'.$channel['remotePort'])?>" class='hide' title="Host Unreachable!"
+                                               data-html="true" data-toggle="tooltip" data-placement="top"></i>
                                             <a href='http://<?= $channel['remoteHost'] ?>/'><?= $channel['remoteHost'] ?></a> by port <?= $channel['localPort'] ?> - <b><?= $channel['comment']?></b>
                                             <a id="" class="btn btn-xs btn-default right glyphicon glyphicon-comment pull-right" href="./home/getScript/BAT/<?=urlencode(base64_encode(json_encode(array(
                                                     'site' => $tunnel,
